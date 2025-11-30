ce031ba043b670b654f207430c6868c17c699c03
Amir Jamshidi <amirjamshidi.developer@gmail.com>
2025-11-30 09:19:44 +0330

    add new log method using logger to app with Centralized logging (structured, timestamped, leveled)

---
+++ src/logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+
+_LEVELS = {
+    "CRITICAL": logging.CRITICAL,
+    "ERROR": logging.ERROR,
+    "WARNING": logging.WARNING,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG,
+}
+
+def get_logger(name: str = "tms_etl") -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+
+    level = _LEVELS.get(os.getenv("LOG_LEVEL", "INFO").upper(), logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    fmt = logging.Formatter(
+        fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+    )
+    handler.setFormatter(fmt)
+
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    logger.propagate = False
+    return logger
