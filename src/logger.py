ae29c8ce04dbfae1030c2f7a8e4085ecc2fee8d3
Amir Jamshidi <amirjamshidi.developer@gmail.com>
2025-12-03 13:16:27 +0330

    update src/logger , customize set level to INFO

--- src/logger.py
+++ src/logger.py
@@ -14,17 +14,14 @@ def get_logger(name: str = "tms_etl") -> logging.Logger:
     logger = logging.getLogger(name)
     if logger.handlers:
         return logger
-
-    level = _LEVELS.get(os.getenv("LOG_LEVEL", "INFO").upper(), logging.INFO)
-
+    
     handler = logging.StreamHandler(sys.stdout)
     format = logging.Formatter(
         fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt="%Y-%m-%dT%H:%M:%S%z",
     )
     handler.setFormatter(format)
-
-    logger.setLevel(level)
+    logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     logger.propagate = False
     return logger
