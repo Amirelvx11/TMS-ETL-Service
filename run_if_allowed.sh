3447fd4ad7163af850d6fac6100a75a0d213a35a
Amir Jamshidi <amirjamshidi.developer@gmail.com>
2025-12-06 08:18:20 +0330

    Update run_if_allowed.sh, remove some comments & change log message

--- run_if_allowed.sh
+++ run_if_allowed.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Current hour (00–23)
 HOUR=$(date +%H)
 
-# Allowed window: 08 ≤ hour < 20
 if [ "$HOUR" -ge 8 ] && [ "$HOUR" -lt 20 ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Running ETL" >> /var/log/etl_scheduler.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - ETL Process Started!" >> /var/log/etl_scheduler.log
     /usr/local/bin/python /app/main.py >> /var/log/etl.log 2>&1
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Outside allowed hours, ETL skipped" >> /var/log/etl_scheduler.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Outside allowed hours, ETL Process skipped" >> /var/log/etl_scheduler.log
 fi
