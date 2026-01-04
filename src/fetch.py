2a62d065659504ab518ccdd6e6f83e4f67edcc7f
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-01-04 12:18:19 +0330

    merged confclit in src/fetch

--- src/fetch.py
+++ src/fetch.py
@@ -108,9 +108,5 @@ def fetch_source_rows(last_id: int) -> pd.DataFrame:
         return df
     except SQLAlchemyError as e:
         log.error(f"Error occurred while fetching rows from source: {e}")
-<<<<<<< Updated upstream
-        return pd.DataFrame() 
-=======
-        return pd.DataFrame()
 
->>>>>>> Stashed changes
+        return pd.DataFrame() 
