1825462b313b446ecde819af634836eb6a867abd
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-01-11 15:21:45 +0330

    change src/config, add some new methods for engine creation (MYSQL) & some minimal changes.

--- src/config.py
+++ src/config.py
@@ -2,22 +2,31 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
+
 load_dotenv()
 
+
 def _require_env(key: str) -> str:
-    """Ensure required environment variables are present."""
+    """Ensure required environment variables are present & passed."""
     value = os.getenv(key)
     if not value:
         raise RuntimeError(f"Missing required environment variable: {key}")
     return value
 
 # Required ENV variables
-SOURCE_DB = _require_env("SOURCE_DB") # pyodbc SQLAlchemy URI
-TARGET_DB = _require_env("TARGET_DB") # pyodbc SQLAlchemy URI
-USER_GUID = _require_env("USER_GUID") # MUST be valid in system.User.Id
+SOURCE_DB = _require_env("SOURCE_DB") # MYSQL SQLAlchemy URI
+TARGET_DB = _require_env("TARGET_DB") # MSSQL+pyodbc SQLAlchemy URI
+USER_GUID = _require_env("USER_GUID") # ADMIN GUID (for data submission)
 
 # Engines (MySQL and MSSQL)
-src_engine = create_engine(SOURCE_DB, pool_pre_ping=True)
+src_engine = create_engine(
+    SOURCE_DB,
+    pool_pre_ping=True,
+    max_overflow=5,
+    pool_timeout=5,
+    pool_recycle=3600,
+    echo=True,
+)
 
 dst_engine = create_engine(
     TARGET_DB,
@@ -25,6 +34,6 @@ dst_engine = create_engine(
     fast_executemany=True,
     max_overflow=5,
     pool_timeout=5,
-    pool_recycle=1800,
+    pool_recycle=3600,
     connect_args={"timeout": 5, "LoginTimeout": 5},
 )
