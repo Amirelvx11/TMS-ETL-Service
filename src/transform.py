bb48630d4562580992196fb6efc54eaa37708f1d
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-01-29 12:04:14 +0330

    change src/transform, remove some blank lines & unnecessary comments

--- src/transform.py
+++ src/transform.py
@@ -23,20 +23,15 @@ def transform_products(df, os_map, mgr_map_exact, mgr_map_short):
 
     for _, row in df.iterrows():
 
-        # Serial Number
         sn = str(row.get("sn") or "").strip()
-
-        # tusn (complete number of serial number)
         tusn = str(row.get("tusn") or "").strip()
-
-        # PartId rules
+        
         part_id = ""
         if sn.startswith("00"):
             part_id = "A3925DD2-F7C3-4E27-B487-E547F8F980E2"
         elif sn.startswith("05"):
             part_id = "B159B8DA-AD61-4C25-97C8-C82CF7955D06"
 
-        # IMEI parsing
         imei_str = str(row.get("imei") or "").strip()
         imei1, imei2 = "0", "0"
         if "," in imei_str:
@@ -45,7 +40,6 @@ def transform_products(df, os_map, mgr_map_exact, mgr_map_short):
         elif imei_str.isdigit():
             imei1 = imei_str
 
-        # OS ID
         cos_raw = str(row.get("cosver") or "")
         cos_norm = normalize_os(cos_raw)
         os_id = os_map.get(cos_norm)
@@ -53,8 +47,7 @@ def transform_products(df, os_map, mgr_map_exact, mgr_map_short):
             os_id = ensure_version_exists_os(cos_raw)
             if os_id:
                 os_map[cos_norm] = os_id
-
-        # Manager ID
+                
         lib_raw = str(row.get("libver") or "")
         ex = manager_exact(lib_raw)
         sh = manager_short(lib_raw)
