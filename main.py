34d2de27def064d4a0a4240761a0364bee2ba7ce
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-02-15 12:26:58 +0330

    change main.py, use correct method for logging in production for silence & prevent abuse & noisy loggigns and also add new os & manager version check method.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import time
-from src.fetch import get_last_tms_id, fetch_lookup_maps, fetch_source_rows
+from src.fetch import (
+get_last_tms_id, fetch_lookup_maps,
+resolve_missing_versions, fetch_source_rows
+)
 from src.transform import transform_products
 from src.insert import insert_products, insert_guaranty
 from backend_toolkit.logger import get_logger
@@ -10,22 +13,23 @@ def run():
     """Main function to run the ETL pipeline."""
     try:
         start_ts = time.monotonic()
-
         start_last_id = get_last_tms_id()
-        logger.info(
-            "ETL cycle started",
-            extra={"start_tms_id": start_last_id},
-        )
         
         df_src = fetch_source_rows(start_last_id)
         if df_src.empty:
             logger.info(
-                "ETL cycle finished - no new data",
+                "ETL cycle finished - No New Data",
                 extra={"start_tms_id": start_last_id},
             )
             return
 
+        logger.info(
+            "ETL cycle started",
+            extra={"start_tms_id": start_last_id},
+        )
+
         os_map, mgr_exact, mgr_short = fetch_lookup_maps()
+        resolve_missing_versions(df_src, os_map, mgr_exact, mgr_short)
         df_prod = transform_products(df_src, os_map, mgr_exact, mgr_short)
 
         inserted_products = insert_products(df_prod)
