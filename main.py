6105b4305c56649de2689f5ac59e834ac4b1805c
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-02-17 13:45:59 +0330

    change main, add both duplicate serial cleanup & hamta code sync inside main etl loop & add exception info in error handling when etl crashes.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import time
 from src.fetch import (
-get_last_tms_id, fetch_lookup_maps,
-resolve_missing_versions, fetch_source_rows
+    get_last_tms_id, fetch_lookup_maps,
+    resolve_missing_versions, fetch_source_rows
 )
 from src.transform import transform_products
 from src.insert import insert_products, insert_guaranty
+from tools.sync_hamta_code import sync_hamta_code
+from tools.cleanup_duplicates import cleanup_duplicate_products
 from backend_toolkit.logger import get_logger
 
-logger = get_logger("etl")
+logger = get_logger("main-etl")
+
 
 def run():
     """Main function to run the ETL pipeline."""
     try:
         start_ts = time.monotonic()
+        cleanup_duplicate_products()
         start_last_id = get_last_tms_id()
 
         df_src = fetch_source_rows(start_last_id)
@@ -20,11 +24,16 @@ def run():
             return
 
         os_map, mgr_exact, mgr_short = fetch_lookup_maps()
-        resolve_missing_versions(df_src, os_map, mgr_exact, mgr_short)
+        resolve_missing_versions(df_src, 
+                                 os_map, mgr_exact, mgr_short)
         df_prod = transform_products(df_src, os_map, mgr_exact, mgr_short)
 
         inserted_products = insert_products(df_prod)
         inserted_guaranty = insert_guaranty(df_prod) if inserted_products else 0
+
+        hamta_synced = 0
+        if inserted_products > 0:
+            hamta_synced = sync_hamta_code(start_last_id)
 
         duration = round(time.monotonic() - start_ts, 3)
 
@@ -32,15 +41,16 @@ def run():
             "ETL cycle completed",
             extra={
                 "start_tms_id": start_last_id,
-                "last_tms_id": int(df_prod["TmsId"].max()),
+                "last_tms_id": int(df_prod["TmsId"].max()) if not df_prod.empty else start_last_id,
                 "fetched": len(df_src),
                 "inserted_products": inserted_products,
                 "inserted_guaranty": inserted_guaranty,
+                "hamta_synced": hamta_synced,
                 "duration_sec": duration,
             },
         )
     except Exception as e:
-        logger.critical("main etl cycle error")
+        logger.critical("main etl cycle crashed",exc_info=True)
         raise
 
 
